86d4d93b32b5d6c559f876da637af1af7813b5a4
jaccobroere <jacbroere@gmail.com>
2023-04-02 16:17:34 +0200

    comments

--- src/yule_walker.jl
+++ src/yule_walker.jl
@@ -11,6 +11,10 @@ using Statistics
 # Set random seed
 Random.seed!(2023)
 
+"""
+Calculate covariance matrix of lag 0, this assumes that the time index is over the columns.
+Thus, y is a N x T matrix, where N is the number of variables and T is the number of time periods.
+"""
 function calc_Σ0(y::Matrix{Float64}, bias::Bool=false)::Matrix{Float64}
     """Calculate covariance matrix of lag 0, this assumes that the time index is over the columns.
     Thus, y is a N x T matrix, where N is the number of variables and T is the number of time periods.
@@ -19,11 +23,11 @@ function calc_Σ0(y::Matrix{Float64}, bias::Bool=false)::Matrix{Float64}
     return cov(y, dims=2, corrected=bias)
 end
 
-
+"""
+Calculate covariance matrix of lag 1, this assumes that the time index is over the columns.
+Thus, y is a N x T matrix, where N is the number of variables and T is the number of time periods.
+"""
 function calc_Σ1(y::Matrix{Float64}, bias::Bool=false)::Matrix{Float64}
-    """
-    Calculate covariance matrix of lag 1, this assumes that the time index is over the columns.
-    Thus, y is a N x T matrix, where N is the number of variables and T is the number of time periods."""
     # Calculate covariance matrix of lag 1
     return cov(y[:, 1:end-1], y[:, 2:end], dims=2, corrected=bias)
 end

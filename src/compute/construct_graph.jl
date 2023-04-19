fa6eccff7466ca713f802e231669cffa00975300
jaccobroere <jacbroere@gmail.com>
2023-04-19 11:32:27 +0200

    fixed the confusion about bandwidths

--- src/compute/construct_graph.jl
+++ src/compute/construct_graph.jl
@@ -6,19 +6,13 @@ include(joinpath(path, "utils.jl"))
 """
 Create an edge between two vertices in the graph.
 
-Parameters
-----------
-i : Int
-    The index of the first vertex.
-p : Int
-    The dimension of the square matrix.
-h : Int
-    The bandwidth of the matrix.
+Args:
+i (int): The index of the first vertex.
+p (int): The dimension of the square matrix.
+h (int): The bandwidth of the matrix.
 
-Returns
--------
-Tuple{Int, Int}
-    A tuple of two integers representing the indices of the two vertices connected by the edge.
+Returns:
+Union{Tuple{Int, Int}, Nothing}: A tuple of two integers representing the indices of the two vertices connected by the edge. If an edge cannot be created between the vertices, returns nothing.
 """
 function create_edge(i::Int, p::Int, h::Int)::Union{Tuple{Int,Int},Nothing}
     if is_edge_case(i, p, h)
@@ -52,17 +46,12 @@ end
 """
 Create a G-SPLASH graph for a given system of equations.
 
-Parameters
-----------
-p : Int
-    The dimension of the square matrix.
-h : Int
-    The bandwidth of the matrix.
+Args:
+p (int): The dimension of the square matrix.
+h (int): The bandwidth of the matrix.
 
-Returns
--------
-SimpleGraphs.SimpleGraph{Int}
-    A G-SPLASH graph representing the system of equations.
+Returns:
+SimpleGraphs.SimpleGraph{Int}: A G-SPLASH graph representing the system of equations.
 """
 function create_gsplash_graph(p::Int, h::Int)::SimpleGraph{Int}
     # Define the maximum numbers of vertices
@@ -81,12 +70,10 @@ function create_gsplash_graph(p::Int, h::Int)::SimpleGraph{Int}
 end
 
 """
-Prints out a list of edges for the given graph `g`.
+Prints out a list of edges for the given graph.
 
-Parameters
-----------
-g : AbstractGraph
-    The graph to print the edges of.
+Args:
+g (AbstractGraph): The graph to print the edges of.
 """
 function print_edges(g::AbstractGraph)
     for edge in edges(g)
@@ -97,14 +84,8 @@ end
 """
 Prints the vertices of the given graph.
 
-Parameters
-----------
-g : AbstractGraph
-    The graph to print the vertices of.
-
-Returns
--------
-None
+Args:
+g (AbstractGraph{T}): The graph to print the vertices of.
 """
 function print_vertices(g::AbstractGraph{T}) where {T}
     for v in vertices(g)
@@ -116,12 +97,9 @@ end
 """
 Save a G-SPLASH graph to a file in GraphML format.
 
-Parameters
-----------
-graph : SimpleGraphs.SimpleGraph
-    The G-SPLASH graph to save.
-path: str, optional
-    The path to save the graph to. Defaults to "graph.graphml".
+Args:
+graph (SimpleGraphs.SimpleGraph): The G-SPLASH graph to save.
+path (str, optional): The path to save the graph to. Defaults to "graph.graphml".
 """
 function save_graph_as_gml(graph, path::AbstractString="graph.graphml")
     savegraph(path, graph, GraphIO.GraphMLFormat())
--- src/compute/construct_transformations.jl
+++ src/compute/construct_transformations.jl
@@ -24,11 +24,11 @@ include(joinpath(path, "construct_graph.jl"))
 """
 Read data from a csv file and return a matrix.
 
-Args:
-    path (str): path to the csv file.
+## Arguments
+- `path::String`: path to the csv file.
 
-Returns:
-    Matrix{Float64}: matrix of shape (N, T) where N is the number of variables and T is the number of time periods.
+## Returns
+- `Matrix{Float64}`: matrix of shape (N, T) where N is the number of variables and T is the number of time periods.
 """
 function read_data(path::String)::Matrix{Float64}
     return Matrix(CSV.read(path, DataFrame))
@@ -39,33 +39,29 @@ end
 Calculate covariance matrix of lag j, this assumes that the time index is over the columns.
 Thus, y is a N x T matrix, where N is the number of variables and T is the number of time periods.
 
-Args:
-    y (Matrix{Float64}): matrix of shape (N, T) where N is the number of variables and T is the number of time periods.
-    j (int): number of lags.
-    bias (bool): whether to use bias correction in covariance calculation.
+## Arguments
+- `y::Matrix{Float64}`: matrix of shape (N, T) where N is the number of variables and T is the number of time periods.
+- `j::int`: number of lags.
+- `bias::bool=false`: whether to use bias correction in covariance calculation.
 
-Returns:
-    Matrix{Float64}: covariance matrix of lag j with shape (N, N).
+## Returns
+- `Matrix{Float64}`: covariance matrix of lag j with shape (N, N).
 """
 function calc_Σj(y::Matrix{Float64}, j::Int, bias::Bool=false)::Matrix{Float64}
     # Calculate covariance matrix of lag j
     return cov(y[:, 1:end-j], y[:, j+1:end], dims=2, corrected=bias)
 end
 
+
 """
 Return a banded matrix with bandwidth h on both sides of the main diagonal.
 
-Parameters
-----------
-A : Matrix{Float64}
-    The input matrix.
-h : int
-    The half-bandwidth to use.
+## Arguments
+- `A::Matrix{Float64}`: The input matrix.
+- `h::int`: The half-bandwidth to use.
 
-Returns
--------
-Matrix{Float64}
-    A banded matrix with bandwidth h on both sides of the main diagonal.
+## Returns
+- `Matrix{Float64}`: A banded matrix with bandwidth h on both sides of the main diagonal.
 """
 function band_matrix(A::Matrix{Float64}, h::Int)::Matrix{Float64}
     N, T = size(A)
@@ -81,58 +77,40 @@ end
 """
 Construct the V matrix containing the columns of C' = [A B]', that are nonzero.
 
-Parameters
-----------
-Σ0 : Matrix{Float64}
-    The covariance matrix of y.
-Σ1 : Matrix{Float64}
-    The covariance matrix of lag 1.
-h : int, optional
-    The half-bandwidth to use. Default is size(Σ1, 1)/4.
-prebanded : bool, optional
-    Whether Σ0 and Σ1 are already banded. Default is false.
+## Arguments
+- `Σ0::Matrix{Float64}`: The covariance matrix of y.
+- `Σ1::Matrix{Float64}`: The covariance matrix of lag 1.
+- `h::int=0`: The half-bandwidth to use. Default is size(Σ1, 1)/4.
+- `prebanded::bool=false`: Whether Σ0 and Σ1 are already banded. Default is false.
 
-Returns
--------
-Matrix{Float64}
-    The V matrix with dimensions N x (N+1), containing the columns of C' = [A B]', that are nonzero.
+## Returns
+- `Matrix{Float64}`: The V matrix with dimensions N x (N+1), containing the columns of C' = [A B]', that are nonzero.
 """
-function constr_Vhat(Σ0::Matrix{Float64}, Σ1::Matrix{Float64}; h::Int=0, prebanded::Bool=false)::Matrix{Float64}
-    if !prebanded
-        if h == 0
-            h = div(size(Σ0, 1), 4)
-        end
-
-        Σ1 = band_matrix(Σ1, h)
-        Σ0 = band_matrix(Σ0, h)
+function constr_Vhat(Σ0::Matrix{Float64}, Σ1::Matrix{Float64}; h0::Int=0, h1::Int=0)::Matrix{Float64}
+    if h0 == 0 || h1 == 0
+        return [Σ1' Σ0]
     end
+
+    Σ0 = band_matrix(Σ0, h0)
+    Σ1 = band_matrix(Σ1, h1)
     return [Σ1' Σ0]
 end
 
 """
 Construct the vectorized autocovariance of lag 1 from the banded autocovariance matrix.
 
-Parameters
-----------
-Σ1 : Matrix{Float64}
-    The banded autocovariance matrix of lag 1.
-prebanded : bool, optional
-    Whether Σ1 is already banded. Default is false.
-h : int, optional
-    The half-bandwidth to use. Default is size(Σ1, 1)/4.
+## Arguments
+- `Σ1::Matrix{Float64}`: The banded autocovariance matrix of lag 1.
+- `h::Int=0`: The half-bandwidth to use. Default is size(Σ1, 1)/4.
 
-Returns
--------
-Vector{Float64}
-    The vectorized autocovariance of lag 1.
+## Returns
+- `Vector{Float64}`: The vectorized autocovariance of lag 1.
 """
-function vec_sigma_h(Σ1::Matrix{Float64}; h::Int=0, prebanded::Bool=false)::Vector{Float64}
-    if !prebanded
-        if h == 0
-            h = div(size(Σ1, 1), 4)
-        end
-        Σ1 = band_matrix(Σ1, h)
+function vec_sigma_h(Σ1::Matrix{Float64}; h1::Int=0)::Vector{Float64}
+    if h1 == 0
+        return vec(Σ1')
     end
+    Σ1 = band_matrix(Σ1, h1)
     return vec(Σ1')
 end
 
@@ -140,29 +118,24 @@ end
 """
 Calculate the active column indices for the matrix V_h^(d).
 
-Parameters
-----------
-p : int
-    The dimension of the matrix V.
-h : int, optional
-    The bandwidth to use. Default is p/4.
+## Arguments
+- `p::Int`: The dimension of the matrix V.
+- `bandwidth::Int=0`: The bandwidth to use. Default is p/4.
 
-Returns
--------
-Vector{Vector{Bool}}
-    A p-length vector containing Boolean vectors representing the active column indices for each row.
+## Returns
+- `Vector{Vector{Bool}}`: A p-length vector containing Boolean vectors representing the active column indices for each row.
 """
-function active_cols(p::Int, h::Int=0)::Vector{Vector{Bool}}
-    if h == 0
-        h = div(p, 4)
+function active_cols(p::Int, bandwidth::Int=0)::Vector{Vector{Bool}}
+    if bandwidth == 0
+        bandwidth = div(p, 4)
     end
 
     active_set = [zeros(Bool, p * 2) for _ in 1:p]
 
     for i in 1:p
-        lower_a = collect(max(1, i - h):max(0, i - 1))
-        upper_a = collect((i+1):min(i + h, p))
-        full_b = collect((p+max(1, (i - h))):(p+min(i + h, p)))
+        lower_a = collect(max(1, i - bandwidth):max(0, i - 1))
+        upper_a = collect((i+1):min(i + bandwidth, p))
+        full_b = collect((p+max(1, (i - bandwidth))):(p+min(i + bandwidth, p)))
         selection = vcat(lower_a, upper_a, full_b)
 
         setindex!(active_set[i], ones(Bool, length(selection)), selection)
@@ -173,19 +146,16 @@ end
 """
 Construct a diagonal block matrix from the active columns of V.
 
-Parameters
-----------
-V : Matrix{Float64}
-    The V matrix with dimensions N x (N+1), containing the columns of C' = [A B]', that are nonzero.
+## Arguments
+- `V::Matrix{Float64}`: The V matrix with dimensions N x (N+1), containing the columns of C' = [A B]', that are nonzero.
+- `bandwidth::Int=0`: The half-bandwidth to use. Default is size(V, 1)/4.
 
-Returns
--------
-SparseMatrixCSC{Float64}
-    The resulting diagonal block matrix with dimensions p^2 x K, where K is the number of nonzero columns in V.
+## Returns
+- `SparseMatrixCSC{Float64}`: The resulting diagonal block matrix with dimensions p^2 x K, where K is the number of nonzero columns in V.
 """
-function constr_Vhat_d(V::Matrix{Float64}, h::Int=0)::SparseMatrixCSC{Float64}
+function constr_Vhat_d(V::Matrix{Float64}, bandwidth::Int=0)::SparseMatrixCSC{Float64}
     p = size(V, 1)
-    active = active_cols(p, h)
+    active = active_cols(p, bandwidth)
     res = [V[:, active[i]] for i in 1:p]
     Vhat_d = spzeros(p^2, sum(sum(active)))
 
@@ -201,17 +171,12 @@ end
 """
 Estimate the covariance matrix of lag j using a bootstrap method (Guo et al. 2016).
 
-Parameters
-----------
-y : Matrix{Float64}
-    The data matrix with dimensions N x T.
-j : int
-    The lag of the covariance matrix to estimate.
+## Arguments
+- `y::Matrix{Float64}`: The data matrix with dimensions N x T.
+- `j::Int`: The lag of the covariance matrix to estimate.
 
-Returns
--------
-Matrix{Float64}
-    The estimated covariance matrix of lag j with dimensions N x N.
+## Returns
+- `Matrix{Float64}`: The estimated covariance matrix of lag j with dimensions N x N.
 """
 function bootstrap_estimator_Σj(y::Matrix{Float64}, j::Int)::Matrix{Float64}
     N, T = size(y)
@@ -223,36 +188,32 @@ function bootstrap_estimator_Σj(y::Matrix{Float64}, j::Int)::Matrix{Float64}
     return Σj / T
 end
 
+
 """
 Estimate the bandwidth for banded autocovariance estimation using a bootstrap method (Guo et al. 2016).
 
-Parameters
-----------
-y : Matrix{Float64}
-    The data matrix with dimensions N x T.
-j : int
-    The lag of the covariance matrix to use in the estimation.
-q : int, optional
-    The number of bootstrap samples to use. Default is 500.
+## Arguments
+- `y::Matrix{Float64}`: The data matrix with dimensions N x T.
+- `q::Int=500`: The number of bootstrap samples to use. Default is 500.
 
-Returns
--------
-int
-    The estimated bandwidth.
+## Returns
+- `Tuple{Int, Int}`: The estimated bandwidths (h_Σ0, h_Σ1).
 """
-function bootstrap_estimator_R(y::Matrix{Float64}, q::Int=500)::Int
+function bootstrap_estimator_R(y::Matrix{Float64}, q::Int=500)::Tuple{Int,Int}
     N, T = size(y)
     Σ0 = calc_Σj(y, 0)
     Σ1 = calc_Σj(y, 1)
-    R = zeros(Float64, q, div(N, 4))
+    R0 = zeros(Float64, q, div(N, 4))
+    R1 = zeros(Float64, q, div(N, 4))
     Threads.@threads for i in 1:q
         bootstrap_Σ0 = bootstrap_estimator_Σj(y, 0)
         bootstrap_Σ1 = bootstrap_estimator_Σj(y, 1)
         for h in 1:div(N, 4)
-            @inbounds R[i, h] += norm((band_matrix(bootstrap_Σ0, h) - Σ0), 1) + norm((band_matrix(bootstrap_Σ1, h) - Σ1), 1)
+            @inbounds R0[i, h] += norm((band_matrix(bootstrap_Σ0, h) - Σ0), 1)
+            @inbounds R1[i, h] += norm((band_matrix(bootstrap_Σ1, h) - Σ1), 1)
         end
     end
-    return argmin(vec(sum(R, dims=1)))
+    return (argmin(vec(sum(R0, dims=1))), argmin(vec(sum(R1, dims=1))))
 end
 
 function main(prefix)
@@ -260,15 +221,15 @@ function main(prefix)
     y = read_data(joinpath("data", "simulation", "$(prefix)_y.csv"))
 
     # Bootstrap the bandwidth
-    h = bootstrap_estimator_R(y, 500)
+    h1, h2 = bootstrap_estimator_R(y, 500)
 
     # Do calculations
     Σ1 = calc_Σj(y, 1)
     Σ0 = calc_Σj(y, 0)
 
-    Vhat = constr_Vhat(Σ0, Σ1, h=h)
-    sigma_hat = vec_sigma_h(Σ1, h=h)
-    Vhat_d = constr_Vhat_d(Vhat, h)
+    Vhat = constr_Vhat(Σ0, Σ1, h0, h1)
+    sigma_hat = vec_sigma_h(Σ1, h1)
+    Vhat_d = constr_Vhat_d(Vhat)
 
     # Construct underlying graph
     graph = create_gsplash_graph(size(y, 1), h)
@@ -286,3 +247,5 @@ main(ARGS[1])
 
 ## TESTING
 y = read_data(joinpath("data", "simulation", "designA_T500_p100_y.csv"))
+
+bootstrap_estimator_R(y, 500)

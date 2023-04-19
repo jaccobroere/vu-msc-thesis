ec9f6808af3722f9cbd300662e477748dfeea435
jaccobroere <jacbroere@gmail.com>
2023-04-19 19:27:36 +0200

    larger run of both algorithms

--- src/playgrounds/playground.r
+++ src/playgrounds/playground.r
@@ -9,7 +9,7 @@ library(tictoc)
 data_dir <- "/Users/jacco/Documents/repos/vu-msc-thesis/data/simulation/"
 out_dir <- "/Users/jacco/Documents/repos/vu-msc-thesis/out/simulation/coef/"
 
-path_prefix <- "designB_T500_p25"
+path_prefix <- "designB_T1000_p100"
 
 # Parse paths
 path1 <- paste0(data_dir, path_prefix, "_sigma_hat.csv")
@@ -32,6 +32,8 @@ y <- as.matrix(fread(path6, header = T, skip = 0))
 # Print the dimensions of the data
 message(cat("The dimension of y: ", dim(y)[1], dim(y)[2]))
 
+# Set the regularization parameter
+lambda <- 0.01
 # Fit the a single solution using (Augmented) ADMM
 gsplash <- admm_gsplash(sigma_hat, Vhat_d, gr, lambda, 1, standard_ADMM = TRUE)
 
@@ -45,7 +47,7 @@ fwrite(data.table(splash$A), file = paste0(out_dir, path_prefix, "_splash_estima
 fwrite(data.table(splash$B), file = paste0(out_dir, path_prefix, "_splash_estimate_B.csv"))
 
 
-model_splash <- splash(t(y), banded_covs = c(FALSE, FALSE), alphas = c(0.5), lambdas = c(lambda))
+# model_splash <- splash(t(y), banded_covs = c(FALSE, FALSE), alphas = c(0.5), lambdas = c(lambda))
 
 
 AB <- model_splash$AB

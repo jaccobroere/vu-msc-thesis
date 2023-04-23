edcdd5d386c9cff4ae1e7d91c4c856bee6312623
jaccobroere <jacbroere@gmail.com>
2023-04-24 00:03:12 +0200

    Much more efficient implementation of graph construction

--- src/compute/construct_graph.jl
+++ src/compute/construct_graph.jl
@@ -4,44 +4,94 @@ path = dirname(abspath(@__FILE__))
 include(joinpath(path, "utils.jl"))
 
 """
-Create an edge between two vertices in the graph.
+generate_idx_mapping(p::Int, bandwidth::Int) -> Dict
 
-Args:
-i (int): The index of the first vertex.
-p (int): The dimension of the square matrix.
-h (int): The bandwidth of the matrix.
+Generate a dictionary that maps the indices of two matrices A and B to the 
+indices of a vector c. The mapping is determined by the size of the matrices 
+(p) and the maximum distance allowed between the indices (bandwidth).
+
+Arguments:
+- `p::Int`: The size of the matrices A and B.
+- `bandwidth::Int`: The maximum distance allowed between the indices.
 
 Returns:
-Union{Tuple{Int, Int}, Nothing}: A tuple of two integers representing the indices of the two vertices connected by the edge. If an edge cannot be created between the vertices, returns nothing.
+- A dictionary that maps a tuple of two indices (i, j) to an integer index in c.
 """
-function create_edge(i::Int, p::Int, h::Int)::Union{Tuple{Int,Int},Nothing}
-    if is_edge_case(i, p, h)
+function generate_idx_mapping(p::Int, bandwidth::Int)::Dict{Tuple{Int,Int},Int}
+    mapping_dict = Dict{Tuple{Int,Int},Int}()
+    idx = 1
+    for i = 1:p
+        for j = 1:2p
+            if j <= p # Belongs to A
+                if abs(i - j) <= bandwidth && i != j
+                    mapping_dict[(i, j)] = idx
+                    idx += 1
+                    continue
+                end
+            else # Belongs to B
+                if abs(i - abs(j - p)) <= bandwidth
+                    mapping_dict[(i, j)] = idx
+                    idx += 1
+                    continue
+                end
+            end
+        end
+    end
+    return mapping_dict
+end
+
+
+"""
+create_diagonal_edge(i, j, mapping_dict) -> Tuple or nothing
+
+Create a diagonal edge in a graph based on the index mapping between two matrices.
+
+Arguments:
+- `i`: The row index in the matrices.
+- `j`: The column index in the matrices.
+- `mapping_dict`: A dictionary that maps a tuple of two indices (i, j) to an integer index in c.
+
+Returns:
+- A tuple representing a diagonal edge in the graph, or nothing if the edge cannot be created.
+"""
+function create_diagonal_edge(i, j, mapping_dict)
+    current = get(mapping_dict, (i, j), -1)
+    neighbor = get(mapping_dict, (i + 1, j + 1), -1)
+
+    if current == -1 || neighbor == -1
         return nothing
     end
 
-    row_num = row_number_of_element(i, p, h)
-    n_elements_this_row = nonzero_elements_per_equation(row_num, p, h)
-    n_elements_next_row = nonzero_elements_per_equation(row_num + 1, p, h)
-    delta_elements = n_elements_next_row - n_elements_this_row
+    return (current, neighbor)
+end
 
-    if belongs_to_A(i, p, h)
-        if delta_elements > 0
-            correction = -1
-        elseif delta_elements == 0
-            correction = 0
-        else
-            correction = 2
-        end
-    else  # Belongs to B
-        if delta_elements < 0
-            correction = 1
-        else
-            correction = 0
-        end
+
+"""
+create_bidirectional_edge(i, j, mapping_dict) -> Tuple or nothing
+
+Create a bidirectional edge in a graph based on the index mapping between two matrices.
+
+Arguments:
+- `i`: The row index in the matrices.
+- `j`: The column index in the matrices.
+- `mapping_dict`: A dictionary that maps a tuple of two indices (i, j) to an integer index in c.
+
+Returns:
+- A tuple representing a bidirectional edge in the graph, or nothing if the edge cannot be created.
+"""
+function create_bidirectional_edge(i, j, mapping_dict)
+    current = get(mapping_dict, (i, j), -1)
+    neighbor = get(mapping_dict, (j, i), -1)
+
+    if current == -1 || neighbor == -1 || i < j
+        return nothing
     end
 
-    return (i, i + n_elements_this_row + delta_elements + correction)
+    return (current, neighbor)
 end
+
+
+
 
 """
 Create a G-SPLASH graph for a given system of equations.
@@ -53,7 +103,7 @@ h (int): The bandwidth of the matrix.
 Returns:
 SimpleGraphs.SimpleGraph{Int}: A G-SPLASH graph representing the system of equations.
 """
-function create_gsplash_graph(p::Int, h::Int=0)::SimpleGraph{Int}
+function create_gsplash_graph2(p::Int, h::Int=0, bidirectional::Bool=false)::SimpleGraph{Int}
     if h == 0
         h = div(p, 4)
     end
@@ -62,9 +112,23 @@ function create_gsplash_graph(p::Int, h::Int=0)::SimpleGraph{Int}
     M::Int = total_number_nonzero_elements(p, h)
     graph = SimpleGraph(M)
 
+    # Generate the index mapping
+    mapping_dict = generate_idx_mapping(p, h)
+
     # Add edges
-    for i::Int in 1:M
-        edge = create_edge(i, p, h)
+    for key in keys(mapping_dict)
+        i, j = key
+
+        # If bidirectional, add bidirectional edges as well
+        if bidirectional
+            edge = create_bidirectional_edge(i, j, mapping_dict)
+            if edge !== nothing
+                add_edge!(graph, edge)
+            end
+        end
+
+        # Add diagonal edges, always 
+        edge = create_diagonal_edge(i, j, mapping_dict)
         if edge !== nothing
             add_edge!(graph, edge)
         end

c8c4808503fdf67fc6baa62bc6e7fa47afd8ab5f
jaccobroere <jacbroere@gmail.com>
2023-04-30 20:58:22 +0200

    yml for MC simulation

--- scripts/run_k8s_determine_lambda.sh
+++ scripts/run_k8s_determine_lambda.sh
@@ -9,15 +9,15 @@ bash scripts/clear_k8s.sh
 
 kubectl apply -f scripts/k8s/setup_pv.yml
 
-kubectl apply -f scripts/k8s/dataaccess.yml
+kubectl apply -f scripts/k8s/data_access.yml
 
-kubectl wait --for=condition=ready --timeout=30s pod/dataaccess
+kubectl wait --for=condition=ready --timeout=30s pod/data_access
 
 kubectl apply -f scripts/k8s/determine_lambda.yml
 
 kubectl wait --for=condition=complete --timeout=30m job/detlam
 
-kubectl exec -it dataaccess -- bash -c "python3 src/compute/save_best_lambda.py"
+kubectl exec -it data_access -- bash -c "python3 src/compute/save_best_lambda.py"
 
 kubectl wait --timeout=10s
 

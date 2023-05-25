b88fcca6c2f90f19676c8469bcab407a20998b70
jaccobroere <jacbroere@gmail.com>
2023-05-25 20:26:46 +0200

    matrix plot update with shades

--- src/plotting/plot_matrices.py
+++ src/plotting/plot_matrices.py
@@ -47,6 +47,45 @@ def generate_matrices(p: int, h: int) -> tuple:
     return matrix_1, matrix_2
 
 
+# %%
+def generate_matrices_color(p: int, h: int) -> tuple:
+    """
+    Generate two matrices of size p x p where each element is either an integer or 0.
+
+    Parameters
+    ----------
+    p : int
+        The size of the matrices.
+    h : int
+        The maximum distance between non-zero elements in the matrices.
+
+    Returns
+    -------
+    tuple
+        A tuple of two matrices of size p x p.
+
+    """
+    matrix_1 = np.zeros((p, p), dtype=float)
+    matrix_2 = np.zeros((p, p), dtype=float)
+
+    for i in range(p):
+        for j in range(p):
+            if i == j:
+                matrix_1[i, j] = 0.15
+            elif abs(i - j) <= h:
+                matrix_1[i, j] = 0.75 + abs(i - j) * 0.07
+            else:
+                matrix_1[i, j] = 0.15
+
+        for j in range(p):
+            if abs(i - j) <= h:
+                matrix_2[i, j] = 0.75 + abs(i - j) * 0.07
+            else:
+                matrix_2[i, j] = 0.15
+
+    return matrix_1, matrix_2
+
+
 def plot_side_by_side_matrices(p: int, h: int) -> None:
     """
     Create a plot showing two matrices side by side with the non-zero elements numbered.
@@ -64,9 +103,10 @@ def plot_side_by_side_matrices(p: int, h: int) -> None:
 
     """
     matrix_1, matrix_2 = generate_matrices(p, h)
-    matrix_color_1, matrix_color_2 = np.where(matrix_1 == 0, 0.15, 0.85), np.where(
-        matrix_2 == 0, 0.15, 0.85
-    )
+    # matrix_color_1, matrix_color_2 = np.where(matrix_1 == 0, 0.15, 0.85), np.where(
+    #     matrix_2 == 0, 0.15, 0.85
+    # )
+    matrix_color_1, matrix_color_2 = generate_matrices_color(p, h)
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(2 * max(5, p // 2), max(5, p // 2)))
     ax1.matshow(

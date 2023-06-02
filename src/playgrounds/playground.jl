fa944423223e4a3c6b8e0db241fe38a7a8b20288
jaccobroere <jacbroere@gmail.com>
2023-06-02 19:05:05 +0200

    update playground

--- src/playgrounds/playground.jl
+++ src/playgrounds/playground.jl
@@ -119,3 +119,79 @@ null = null_space_graph(graph)
 Dtilde = construct_Dtilde(D, null)
 
 
+graph = create_gsplash_graph(9)
+
+D = Matrix(incidence_matrix(graph, oriented=true)')
+
+
+function generate_diagonal_collection(p::Int, bandwidth::Int)::Dict{Tuple{Int,Char},Vector{Int}}
+    diagonal_collection = Dict{Tuple{Int,Char},Vector{Int}}()
+    idx = 1
+    for i = 1:p
+        for j = 1:2p
+            if j <= p # Belongs to A
+                if abs(i - j) <= bandwidth && i != j
+                    try
+                        push!(diagonal_collection[(i - j, 'A')], idx)
+                    catch
+                        diagonal_collection[(i - j, 'A')] = [idx]
+                    end
+                    idx += 1
+                    continue
+                end
+            else # Belongs to B
+                if abs(i - abs(j - p)) <= bandwidth
+                    try
+                        push!(diagonal_collection[(i - j, 'B')], idx)
+                    catch
+                        diagonal_collection[(i - j, 'B')] = [idx]
+                    end
+                    idx += 1
+                    continue
+                end
+            end
+        end
+    end
+    return diagonal_collection
+end
+
+function entire_diagonal_penalties(D::Matrix{Int64}, p::Int, h::Int)::Matrix{Int}
+    n_diagonals = 4h + 1
+    diagonal_collection = generate_diagonal_collection(p, h)
+    penalties = spzeros(Int, n_diagonals, size(D, 2))
+    for (i, (key, value)) in enumerate(diagonal_collection)
+        setindex!(penalties, ones(Int, length(value)), i, value)
+    end
+
+    return penalties
+end
+
+function calc_Dtilde_SDF(D::Matrix{Int}, p::Int, h::Int)::Matrix{Int}
+    return vcat(D, entire_diagonal_penalties(D, p, h))
+end
+
+
+function Dtilde_per_p(p::Int)
+    h = div(p - 1, 4)
+    graph = create_gsplash_graph(p)
+    D = Matrix(incidence_matrix(graph, oriented=true)')
+    n_diagonals = 4h + 1
+
+    diagonal_collection = generate_diagonal_collection(p, h)
+    EX = entire_diagonal_penalties(D, p, h)
+    Dtilde = calc_Dtilde_SDF(D, p, h)
+
+    try
+        res = inv(Dtilde)
+        print("SUCCESS")
+        return res
+    catch
+        println("p = $p is not invertible")
+        return nothing
+    end
+end
+
+
+for p in 95:100
+    Dtilde = Dtilde_per_p(p)
+end

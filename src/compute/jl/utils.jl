7c65aa7efb03070e7f4ea49159cefcd70d2a0580
jaccobroere <jacbroere@gmail.com>
2023-06-02 19:04:32 +0200

    implementation of SDF-SPLASH algorithm

--- src/compute/jl/utils.jl
+++ src/compute/jl/utils.jl
@@ -167,6 +167,59 @@ function inv_Dtilde_SSF_sparse(graph::SimpleGraph, h::Int=0)::SparseMatrixCSC{Fl
     return sparse(inv(lu(Dtilde)))
 end
 
+function generate_diagonal_collection(p::Int, bandwidth::Int)::Dict{Tuple{Int,Char},Vector{Int}}
+    diagonal_collection = Dict{Tuple{Int,Char},Vector{Int}}()
+    idx = 1
+    for i = 1:p
+        for j = 1:2p
+            if j <= p # Belongs to A
+                if abs(i - j) <= bandwidth && i != j
+                    try
+                        push!(diagonal_collection[(i - j, 'A')], idx)
+                    catch
+                        diagonal_collection[(i - j, 'A')] = [idx]
+                    end
+                    idx += 1
+                    continue
+                end
+            else # Belongs to B
+                if abs(i - abs(j - p)) <= bandwidth
+                    try
+                        push!(diagonal_collection[(i - j, 'B')], idx)
+                    catch
+                        diagonal_collection[(i - j, 'B')] = [idx]
+                    end
+                    idx += 1
+                    continue
+                end
+            end
+        end
+    end
+    return diagonal_collection
+end
+
+function entire_diagonal_penalties(D::Matrix{Int64}, p::Int, h::Int)::Matrix{Int}
+    n_diagonals = 4h + 1
+    diagonal_collection = generate_diagonal_collection(p, h)
+    penalties = spzeros(Int, n_diagonals, size(D, 2))
+    for (i, (key, value)) in enumerate(diagonal_collection)
+        setindex!(penalties, ones(Int, length(value)), i, value)
+    end
+
+    return penalties
+end
+
+function calc_Dtilde_SDF_sparse(graph::SimpleGraph, p::Int, h::Int=0)::SparseMatrixCSC{Float64}
+    D = sparse(incidence_matrix(graph, oriented=true))' # Incidence matrix needs to be transposed before obtaining D^(G)
+    EX = entire_diagonal_penalties(D, p, h)
+    return vcat(D, EX)
+end
+
+function inv_Dtilde_SDF_sparse(graph::SimpleGraph, p::Int, h::Int=0)::SparseMatrixCSC{Float64}
+    Dtilde = calc_Dtilde_SDF_sparse(graph, p, h)
+    return sparse(inv(lu(Dtilde)))
+end
+
 function save_bandwiths_bootstrap(path::String, h0, h1)
     df = DataFrame(h0=[h0], h1=[h1])
     CSV.write(path, df)

2bb3c3201455bfd992efd949b533749a6cd3768e
jaccobroere <jacbroere@gmail.com>
2023-06-22 19:32:52 +0200

    Revert "Revert "large cleanup steps and renames of files""
    This reverts commit 11dc8431a11fbebb2a99000bacd3f0b9ae7f1635.

--- src/simulation/testing.jl
+++
@@ -1,7 +0,0 @@
-using Test
-
-# include(joinpath(dirname(abspath(@__FILE__)), "simulation_designA.jl"))
-include(joinpath(dirname(abspath(@__FILE__)), "simulation_designB.jl"))
-# include(joinpath(dirname(abspath(@__FILE__)), "simulation_designC.jl"))
-
-generate_A(5)

643f6c6899bd78e12d4db5100b0e4c74b52af846
jaccobroere <jacbroere@gmail.com>
2023-06-28 12:50:39 +0200

    full sim_design_id list to reproduce

--- scripts/run_k8s_full_per_design.sh
+++ scripts/run_k8s_full_per_design.sh
@@ -3,7 +3,27 @@
 cd $PROJ_DIR
 
 # Define your list of sim_design_ids
-sim_design_ids=("designC_T500_p36" "designC_T1000_p36" "designB_T500_p36" "designB_T1000_p36")
+sim_design_ids=(
+    "designB_T500_p25"
+    "designB_T500_p36"
+    "designB_T1000_p25"
+    "designB_T1000_p36"
+    "designB_T2000_p25"
+    "designB_T2000_p36"
+    "designC_T500_p25"
+    "designC_T500_p36"
+    "designC_T1000_p25"
+    "designC_T1000_p36"
+    "designC_T2000_p25"
+    "designC_T2000_p36"
+    "designD_T500_p25"
+    "designD_T500_p36"
+    "designD_T1000_p25"
+    "designD_T1000_p36"
+    "designD_T2000_p25"
+    "designD_T2000_p36"
+)
+
 
 # Loop over the array
 for sim_design_id in "${sim_design_ids[@]}"; do
--- scripts/run_local_full_design.sh
+++ scripts/run_local_full_design.sh
@@ -6,7 +6,27 @@ cd $PROJ_DIR
 
 # Insert the design ID into the k8s job YML files
 # sim_design_id=$1
-sim_design_ids=("designC_T1000_p36")
+sim_design_ids=(
+    "designB_T500_p25"
+    "designB_T500_p36"
+    "designB_T1000_p25"
+    "designB_T1000_p36"
+    "designB_T2000_p25"
+    "designB_T2000_p36"
+    "designC_T500_p25"
+    "designC_T500_p36"
+    "designC_T1000_p25"
+    "designC_T1000_p36"
+    "designC_T2000_p25"
+    "designC_T2000_p36"
+    "designD_T500_p25"
+    "designD_T500_p36"
+    "designD_T1000_p25"
+    "designD_T1000_p36"
+    "designD_T2000_p25"
+    "designD_T2000_p36"
+)
+
 # rundetlam=${2:-"false"}
 # replace_string='s/REPLACEME/'$sim_design_id'/g'
 # sim_design_id_dashes=${sim_design_id//_/-}
@@ -24,7 +44,7 @@ sim_design_ids=("designC_T1000_p36")
 # Run the simulation job
 for sim_design_id in "${sim_design_ids[@]}"; do
     echo "STARTING: Running MC simulation for design $sim_design_id"
-    for ((i=1; i<=5; i++))
+    for ((i=1; i<=250; i++))
     do
         # Task to repeat
         bash scripts/model_fit_montecarlo.sh $sim_design_id
